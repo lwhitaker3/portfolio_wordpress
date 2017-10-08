598128bb15402608d0814ea4089595457c6187c7
Conor Reisman <conorreisman@gmail.com>
2017-10-08 00:03:18 -0700

    Disable SVG animations on unsupported browsers.

--- lib/setup.php
+++ lib/setup.php
@@ -102,5 +102,6 @@ function assets() {
   }
   wp_enqueue_script('sage/custom_js', Assets\asset_path('scripts/custom.js'), ['sage/js'], null, true);
   wp_enqueue_script('sage/js', Assets\asset_path('scripts/main.js'), ['jquery'], null, true);
+  wp_enqueue_script('sage/modernizr', Assets\asset_path('scripts/modernizr.js'), [], null, true);
 }
 add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\assets', 100);
